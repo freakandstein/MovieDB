497866526347566b58d641562ef43d2f43f55416
Satrio Wicaksono <satrio.wicaksono.m1.pro@Satrios-MacBook-M1-Pro-14.local>
2022-11-12 11:38:34 +0700

    Add xcconfig
    - Add API Key on xcconfig
    - Add API Key on config helper

--- MovieDB/Helper/ConfigHelper.swift
+++ MovieDB/Helper/ConfigHelper.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+public enum ConfigKey: String {
+  case APIKey
+  
+  var value: String {
+    return self.rawValue
+  }
+}
+
+public protocol ConfigHelperProtocol: AnyObject {
+  func getValue(configKey: ConfigKey) -> String
+}
+
+public class ConfigHelper: ConfigHelperProtocol {
+  
+  public static let shared = ConfigHelper()
+  private var dictionary: [String: Any]?
+  
+  public init() {
+    dictionary = Bundle.main.infoDictionary
+  }
+  
+  public func getValue(configKey: ConfigKey) -> String {
+    guard let configValue = dictionary?[configKey.value] as? String else { return .empty }
+    return configValue.replacingOccurrences(of: "\\", with: String.empty)
+  }
+}
+
