385322f1cdd39513955d029113db64d6c97a733e
Satrio Wicaksono <satrio.wicaksono.m1.pro@Satrios-MacBook-M1-Pro-14.local>
2022-11-13 22:52:24 +0700

    Add data service
    - Add DataService to handle saving data in local

---
+++ MovieDB/Service/Data/DataService.swift
@@ -0,0 +1,42 @@
+//
+//  DataService.swift
+//  MovieDB
+//
+//  Created by Satrio Wicaksono on 13/11/22.
+//
+
+import Foundation
+
+protocol DataServiceProtocol: AnyObject {
+    func save<T: Codable>(data: [T], key: String) throws
+    func load(key: String) throws -> Data
+    func remove(key: String) throws
+}
+
+class DataServiceProvider: DataServiceProtocol {
+    
+    let userDefault = UserDefaults.standard
+    
+    func save<T: Codable>(data: [T], key: String) throws {
+        if let encoded = try? JSONEncoder().encode(data) {
+            userDefault.set(encoded, forKey: key)
+        } else {
+            throw .saveError()
+        }
+    }
+    
+    func load(key: String) throws -> Data {
+        if let data = userDefault.object(forKey: key) as? Data {
+            return data
+        }
+        throw .loadError()
+    }
+    
+    func remove(key: String) throws {
+        userDefault.removeObject(forKey: key)
+    }
+}
+
+class DataService {
+    static let shared: DataServiceProtocol = DataServiceProvider()
+}
